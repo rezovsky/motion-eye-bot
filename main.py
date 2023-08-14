d05319d4aa0e38f39b9f19867857cad8972ad3da
e_rezovsky <e.rezovsky@gmail.com>
2023-08-14 16:39:42 +0500

    gitignore

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import cv2
-import telebot
-import time
 
 # Замените на ваш токен бота
 TOKEN = '6350121672:AAEsGxrOAtBpci8MXpp9nRpZUnVQjQCQyCg'
@@ -8,24 +5,65 @@ TOKEN = '6350121672:AAEsGxrOAtBpci8MXpp9nRpZUnVQjQCQyCg'
 # Замените на ваш чат ID (можно узнать у бота @userinfobot в Телеграме)
 CHAT_ID = '888797603'
 
-bot = telebot.TeleBot(TOKEN)
+import cv2
+import asyncio
+from aiogram import Bot, types
+from aiogram.utils import executor
+import time
+from moviepy.editor import VideoFileClip
+
+
+
+bot = Bot(token=TOKEN)
+loop = asyncio.get_event_loop()
+
+frame_count = 0
 
 def motion_detection(frame, motion_detector, threshold_area):
+    global frame_count
     fg_mask = motion_detector.apply(frame)
     contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    motion_detected = False
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > threshold_area:
-            return True
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Рисуем рамку вокруг объекта
+            motion_detected = True
 
-    return False
+    return frame, motion_detected
 
-def main():
+async def send_motion_photo():
+    with open('motion_photo.jpg', 'rb') as photo:
+        await bot.send_photo(chat_id=CHAT_ID, photo=types.InputFile(photo))
+
+async def send_motion_video():
+    from moviepy.editor import VideoFileClip
+    import os
+
+    # Конвертировать AVI видео в MP4
+    clip = VideoFileClip('motion_detected.avi')
+    clip.write_videofile('motion_detected.mp4', codec='libx264', threads=4)
+
+    # Отправить видео в Телеграм
+    with open('motion_detected.mp4', 'rb') as video:
+        await bot.send_video(chat_id=CHAT_ID, video=types.InputFile(video))
+
+    # Удалить AVI файл после конвертации
+    os.remove('motion_detected.avi')
+
+async def main():
+    global frame_count
     cap = cv2.VideoCapture(0)
     motion_detector = cv2.createBackgroundSubtractorMOG2()
     threshold_area = 1000
-    interval = 10  # Интервал отправки сообщений (в секундах)
+
+    recording = False
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = None
+
+    last_photo_time = 0
     last_send_time = 0
 
     while True:
@@ -33,22 +71,46 @@ def main():
         if not ret:
             break
 
-        if motion_detection(frame, motion_detector, threshold_area):
-            current_time = time.time()
-            if current_time - last_send_time >= interval:
-                last_send_time = current_time
+        current_time = time.time()
 
-                cv2.imwrite('motion_detected.jpg', frame)
-                with open('motion_detected.jpg', 'rb') as photo:
-                    bot.send_photo(CHAT_ID, photo)
+        result_frame, motion_detected = motion_detection(frame, motion_detector, threshold_area)
 
-        cv2.imshow('Motion Detection', frame)
+        if motion_detected:
+            frame_count += 1
+            if frame_count >= 5:  # Отправить кадр после пяти кадров
+                frame_count = 0
+                if not recording:
+                    recording = True
+                    out = cv2.VideoWriter('motion_detected.avi', fourcc, 20.0, (640, 480))
+                    last_send_time = current_time
+
+                out.write(frame)
+
+                if current_time - last_photo_time >= 30:
+                    last_photo_time = current_time
+                    cv2.imwrite('motion_photo.jpg', result_frame)
+                    await send_motion_photo()
+
+        else:
+            frame_count = 0  # Сбрасываем счетчик кадров при прекращении движения
+            if recording:
+                recording = False
+                out.release()
+
+                if current_time - last_send_time >= 30:
+                    await send_motion_video()
+                    last_send_time = current_time
+
+        cv2.imshow('Motion Detection', result_frame)
 
         if cv2.waitKey(30) & 0xFF == 27:
             break
+
+    if out:
+        out.release()
 
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    loop.run_until_complete(main())
