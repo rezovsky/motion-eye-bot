36c4f1de8a17da73a26b42c4a777d64d012b6406
e_rezovsky <e.rezovsky@gmail.com>
2023-08-15 11:36:51 +0500

    edit settings.py

--- settings.py
+++ settings.py
@@ -21,13 +21,16 @@ def get_connected_cameras():
 
 # Проходимся по разделам и параметрам в config.ini
 for section in config.sections():
-    print(f"Section: {section}")
+    print(f"Section: {colored(section, 'light_yellow')}")
     for key, value in config[section].items():
         if key.lower() == 'camera_index':
             connected_cameras = get_connected_cameras()
-            print("Available cameras:")
+            print(colored("Available cameras:", 'light_cyan'))
             for index, name in connected_cameras.items():
-                print(f"{index}: {name}")
+                if index == int(value):
+                    print(colored(f"{index}: {name}", 'green'))
+                else:
+                    print(f"{index}: {name}")
 
             camera_choice = input(
                 f"Enter the index of the camera you want to use (current: {colored(value, 'green')}, press Enter to keep unchanged): ")
@@ -36,7 +39,7 @@ for section in config.sections():
             else:
                 config[section][key] = camera_choice
         else:
-            new_value = input(f"Enter new value for {key} (current: {colored(value, 'green')}, press Enter to keep unchanged): ")
+            new_value = input(f"Enter new value for {colored(key, 'blue')} (current: {colored(value, 'green')}, press Enter to keep unchanged): ")
             if new_value.strip():
                 config[section][key] = new_value
 
