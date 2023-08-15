4c657b1d64c09ef3f708bd5a37c11923810169dc
e_rezovsky <e.rezovsky@gmail.com>
2023-08-15 11:38:32 +0500

    edit settings.py

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-python -m venv venv && .\venv\Scripts\activate && pip install -r .\requarements.txt && .\settings.bat
+python -m venv venv && .\venv\Scripts\activate && pip install -r .\requirements.txt && .\settings.bat
